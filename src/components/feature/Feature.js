5daaa9dddfbf191fa82f122601bd6f6a4d165765
topgiter <devstarstefan@gmail.com>
2018-07-02 17:50:22 -0400

    Added example styles

--- src/components/feature/Feature.js
+++ src/components/feature/Feature.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import './Feature.css';
 
 const propTypes = {
   className: PropTypes.string,
@@ -12,11 +13,15 @@ const defaultProps = {
 class Feature extends Component {
   render() {
     const { className, feature } = this.props;
-
+    const featureClassName = 'feature feature-' + className;
     return (
       <div className="feature-col">
-        <div className="feature">
-          {className}
+        <div className={featureClassName}>
+          <div><span>Name:</span><b>{feature.name}</b></div>
+          <div><span>ParentId:{feature.parent_id}</span></div>
+          <div><span>OwnerId:{feature.owner_id}</span></div>
+          <div><span>StateId:</span><i>{feature.state_id}</i></div>
+          <div><span>...</span></div>
         </div>
       </div>
     );
