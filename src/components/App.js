d844d640e9dc459fc973c5a57f00c03c7dc05cd7
topgiter <devstarstefan@gmail.com>
2018-07-03 01:52:35 -0400

    Adjusted cols and rows

--- src/components/App.js
+++ src/components/App.js
@@ -13,12 +13,23 @@ const defaultProps = {
 };
 
 class App extends Component {
+  calcRows(total) {
+    const minCols = Math.ceil(Math.sqrt(total));
+    const { maxCol } = this.props;
+
+    for (let i = minCols; i <= maxCol; i++) {
+      if (total % i === 0) return total / i;
+    }
+
+    return Math.ceil(total / minCols);
+  }
+
   render() {
     const { maxCol, classNames } = this.props;
     const features = mockData.features.filter(f => f.FEATURE_TYPE === 'feature');
     const sortedFeatures = features.sort((f1, f2) => f1.state_id - f2.state_id);
     const countOfFeatures = sortedFeatures.length;
-    const rows = countOfFeatures >= 20 ? Math.floor(Math.sqrt(countOfFeatures)) : maxCol;
+    const rows = countOfFeatures >= maxCol * maxCol ? Math.ceil(countOfFeatures / maxCol): this.calcRows(countOfFeatures);
     const cols = Math.ceil(countOfFeatures / rows);
 
     let productNode = [];
@@ -26,16 +37,18 @@ class App extends Component {
       let rowNodes = [];
       for (let c = 0; c < cols; c++) {
         const index = r * cols + c;
-        if (index >= countOfFeatures) break;
-
-        const feature = sortedFeatures[index];
-        rowNodes.push(
-          <Feature
-            key={`feature-${feature.id}`}
-            className={classNames[feature.state_id]}
-            feature={feature}
-          />
-        )
+        if (index >= countOfFeatures) {
+          rowNodes.push(<div className="feature-col" key={`feature-${index}`}></div>);
+        } else {
+          const feature = sortedFeatures[index];
+          rowNodes.push(
+            <Feature
+              key={`feature-${feature.id}`}
+              className={classNames[feature.state_id]}
+              feature={feature}
+            />
+          );
+        }
       }
 
       productNode.push(
